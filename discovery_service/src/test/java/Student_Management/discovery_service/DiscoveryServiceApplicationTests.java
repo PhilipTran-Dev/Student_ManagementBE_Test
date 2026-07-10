a97751a3d08a0a24bb17dfe6c9bbaab84119d123
Philip Tran <tnvphuccntt@gmail.com>
2026-07-10 20:53:51 +0700

    first commit

---
+++ discovery_service/src/test/java/Student_Management/discovery_service/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Student_Management.discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
