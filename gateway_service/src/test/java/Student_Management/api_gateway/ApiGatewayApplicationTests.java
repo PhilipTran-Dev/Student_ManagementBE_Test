5538d00ac55427284823459f0247e74b7a476680
Philip Tran <tnvphuccntt@gmail.com>
2026-07-22 22:26:34 +0700

    first commit

---
+++ gateway_service/src/test/java/Student_Management/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package Student_Management.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
